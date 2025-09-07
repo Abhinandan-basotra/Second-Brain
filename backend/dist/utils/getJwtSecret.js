1bcfa953ebeba647ae8c1051ba80a2741a71844d
Abhinandan-basotra <abhinandanbasotra22@gmail.com>
2025-09-07 12:13:51 +0530

    Remove ignored files from repository

--- backend/dist/utils/getJwtSecret.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getJwtSecret = getJwtSecret;
-function getJwtSecret() {
-    if (!process.env.JWT_SECRET_KEY) {
-        throw new Error("JWT secret key is not defined in environment variables.");
-    }
-    return process.env.JWT_SECRET_KEY;
-}
